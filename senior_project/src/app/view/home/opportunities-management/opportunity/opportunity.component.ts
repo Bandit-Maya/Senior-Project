a5c2731a356ada4c0529a0519e52f767bef055d4
John-P-Foster <johnpete.foster@gmail.com>
2024-08-21 21:09:18 -0400

    Volunteers matched to Opportunities based on skills

--- senior_project/src/app/view/home/opportunities-management/opportunity/opportunity.component.ts
+++ senior_project/src/app/view/home/opportunities-management/opportunity/opportunity.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input, Output,EventEmitter, inject} from '@angular/core';
 import { Opportunity } from './opportunity.model';
+import { Volunteer } from '../../../volunteer/volunteer.model';
 import { OpportunitiesManagementService } from '../opportunities-management.service';
 import { EditOpportunityComponent } from '../edit-opportunity/edit-opportunity.component';
+import { VolunteerServiceService } from '../../../volunteer/volunteer-service.service';
+
 //import { EventEmitter } from 'stream';
 
 @Component({
@@ -15,8 +18,23 @@ export class OpportunityComponent {
   @Input ({required: true}) opportunity!: Opportunity;
   @Output() removeId = new EventEmitter(); 
 
-  private OpportunitiesManagementService = inject(OpportunitiesManagementService)
+  //private OpportunitiesManagementService = inject(OpportunitiesManagementService)
+  private volunteerServiceService = inject(VolunteerServiceService);
   isEditingOpportunity = false;
+
+  volunteers: Volunteer [] =[];
+  matchedVolunteers: Volunteer [] = [];
+
+  ngOnInit(){
+    this.volunteers = this.volunteerServiceService.getVolunteers(); 
+    
+    this.opportunity.reqSkills.forEach(skill => {
+      this.matchedVolunteers = this.volunteers.filter((volunteer)=>{
+        let match = volunteer.skills.toLowerCase().includes(skill.value.toLowerCase());
+        return match;
+      })
+    } );
+  }
 
   onRemoveOpportunity(){
     this.removeId.emit(this.opportunity._id);
